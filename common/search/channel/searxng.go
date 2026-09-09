84dec1492fc6cadadecfa0e4afba40b03548b7e1
leviathan <chu.quntao@gmail.com>
2026-09-09 23:11:52 +0800

    fix(search): 按只读查询与执行请求选择 HTTP 策略

--- common/search/channel/searxng.go
+++ common/search/channel/searxng.go
@@ -39,6 +39,7 @@ func (s *Searxng) Query(query string) (*search_type.SearchResponses, error) {
 	queryUrl = strings.Replace(s.Url, "{query}", queryUrl, 1)
 
 	client := requester.NewHTTPRequester("", nil)
+	client.UseHTTPProfile(requester.HTTPProfileObservationGET)
 	client.PrefixProviderErrors = false
 
 	req, err := client.NewRequest(http.MethodGet, queryUrl, client.WithHeader(requester.GetJsonHeaders()))
